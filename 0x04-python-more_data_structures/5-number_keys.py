62277dca93b9b1dfdd9defbd4867f76292263f0b
Vokuc <ukpaival@gmail.com>
2022-07-07 06:47:07 -0700

    number of keys count from dict

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 def number_keys(a_dictionary):
-    key_holder = []
-    for keys in a_dictionary:
-        key_holder.append(keys)
+    num = 0
+    list_keys = list(a_dictionary.keys())
 
-    return "Number of keys: {:d}".format(len(key_holder))
+    for i in list_keys:
+        num += 1
+
+    return (num)
