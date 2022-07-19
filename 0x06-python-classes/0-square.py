45251403a990cc46597237feac54daf6d6a64f48
Vokuc <ukpaival@gmail.com>
2022-07-19 07:39:35 -0700

    create class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-""" Define a class square """
+""" Define a class Square """
+
 
 class Square:
-    """Write an empty class Square that defines a square"""
+    """Represent a square"""
     pass
